2055e3944399d79a9d3154c123048a0d36792abd
songbin <1692876458@qq.com>
2021-01-23 20:43:14 +0800

    模块化博客项目

--- bl-web/src/main/java/com/BlogApplication.java
+++ bl-web/src/main/java/com/BlogApplication.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @MapperScan("mapper")
-@ComponentScan(basePackages = {"service","com.controller"})
+@ComponentScan(basePackages = {"service","com"})
 public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class,args);
